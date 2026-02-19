3ed6667f12ef7a9105cb3cbabab8172208886f79
RahulChaple <rahulchaple2002@gmail.com>
2026-02-19 17:53:56 +0530

    LED blinking with arduino

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,9 +1,16 @@
+// LED blinking in 1 sec
+
 #define LED_PIN 5
 
 void setup() {
+  
   pinMode(LED_PIN, OUTPUT); 
   Serial.begin(9600);
+
 void loop() {
   digitalWrite(LED_PIN, HIGH); 
   Serial.println("LED is ON");
+  delay(1000);
+  digitalWrite(LED_PIN, LOW); 
+  Serial.println("LED is OFF");
 }
