95b02047d374df8cdb32ccbfa8018a423dde378a
RahulChaple <rahulchaple2002@gmail.com>
2026-02-19 18:13:46 +0530

    PIR sensor integration

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,21 +1,22 @@
-// LED blinking in 1 sec
-
 #define LED_PIN 2
 #define buzzer 3
-
+#define PIR_PIN 4 
 void setup() {
-  
-  pinMode(LED_PIN, OUTPUT);
-  pinMode(buzzer, OUTPUT); 
- 
-  Serial.begin(9600);
-
+pinMode(LED_PIN, OUTPUT);
+pinMode(buzzer, OUTPUT);
+pinMode(PIR_PIN, INPUT);
+Serial.begin(9600);
+} 
 void loop() {
-  digitalWrite(LED_PIN, HIGH); 
-  digitalWrite(buzzer, HIGH); 
-  Serial.println("LED and Buzzer is ON");
-  delay(1000);
-  digitalWrite(LED_PIN, LOW); 
-  digitalWrite(buzzer, LOW); 
-  Serial.println("LED and Buzzer is OFF");
+int pirState = digitalRead(PIR_PIN); 
+if (pirState == HIGH) {
+digitalWrite(LED_PIN, HIGH);
+digitalWrite(buzzer, HIGH);
+Serial.println("Motion detected! LED and Buzzer ON");
+} else {
+digitalWrite(LED_PIN, LOW);
+digitalWrite(buzzer, LOW);
+Serial.println("No motion. LED and Buzzer OFF");
 }
+delay(500);
+} 
