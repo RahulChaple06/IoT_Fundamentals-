16643d58a1b39e383428be45fe22666ee328b834
RahulChaple <rahulchaple2002@gmail.com>
2026-02-19 18:06:20 +0530

    LED and buzzer with arduino

--- LED/LED.ino
+++ LED/LED.ino
@@ -1,16 +1,21 @@
 // LED blinking in 1 sec
 
-#define LED_PIN 5
+#define LED_PIN 2
+#define buzzer 3
 
 void setup() {
   
-  pinMode(LED_PIN, OUTPUT); 
+  pinMode(LED_PIN, OUTPUT);
+  pinMode(buzzer, OUTPUT); 
+ 
   Serial.begin(9600);
 
 void loop() {
   digitalWrite(LED_PIN, HIGH); 
-  Serial.println("LED is ON");
+  digitalWrite(buzzer, HIGH); 
+  Serial.println("LED and Buzzer is ON");
   delay(1000);
   digitalWrite(LED_PIN, LOW); 
-  Serial.println("LED is OFF");
+  digitalWrite(buzzer, LOW); 
+  Serial.println("LED and Buzzer is OFF");
 }
